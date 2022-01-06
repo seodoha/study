af636e785b52741650ecae60051197b25637bc55
서도하 <seodoha@reflexion.co.kr>
2022-01-06 13:29:55 +0900

    33

---
+++ vue-project/vue-todo/src/components/TodoFooter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="clearAllContainer">
+        <span class="clearAllBtn" v-on:click="clearTodo">Clear All</span>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        clearTodo() {
+            localStorage.clear();
+        },
+    },
+}
+</script>
+
+<style>
+.clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: #fff;
+    border-radius: 5px;
+    margin: 0 auto;
+}
+.clearAllBtn {
+    display: block;
+    color: #e20303;
+}
+</style>
---
+++ vue-project/vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            newTodoItem: "",
+        }
+    },
+    methods: {
+        addTodo() {
+            if (this.newTodoItem !== '') {
+                var obj = {
+                    completed: false,
+                    item: this.newTodoItem
+                };
+                localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+                this.clearInput();
+            }
+        },
+        clearInput() {
+            this.newTodoItem = '';
+        },
+    },
+}
+</script>
+
+<style scoped>
+input:focus {
+    outline: none;
+}
+.inputBox {
+    background-color: #fff;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+    text-align: left;
+}
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer {
+    text-align: center;
+    float: right;
+    background: linear-gradient(to right, #6478fb, #8763fb);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn {
+    color: #fff;
+    vertical-align: middle;
+}
+</style>
